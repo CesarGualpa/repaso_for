f860c2f5565b9e35740f490be0df23c64855c73d
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-27 11:50:04 -0500

    Ejercicios adicionales

--- repaso.js
+++ repaso.js
@@ -4,12 +4,29 @@ function ejercicio1(){
     }
 }
 
+function ejercicio2(){
+    for(let i = 2; i <= 10; i+=2){
+        console.log(i);
+    }
+}
+
+function ejercicio3(){
+    for(let i = 10; i >= 1; i--){
+        console.log(i);
+    }
+}
+
 function mostrar(opcion){
     switch(opcion){
         case 1:
             ejercicio1();
             break;
-
+        case 2:
+            ejercicio2();
+            break;
+        case 3:
+            ejercicio3();
+            break;
         default:
             console.log("Opción no válida");
     }
