3f7a40bc324c43f189be2e8e0b80e8712e4e012b
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-27 11:39:30 -0500

    Funcion principal

---
+++ repaso.js
@@ -0,0 +1,18 @@
+function mostrar(opcion){
+    switch(opcion){
+        case 1:
+            listarImpares();
+            break;
+
+        case 2:
+            // otraFuncion();
+            break;
+
+        case 3:
+            // otraFuncion();
+            break;
+
+        default:
+            console.log("Opción no válida");
+    }
+}
